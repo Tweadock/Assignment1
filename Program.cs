690199011b28fe4f8eed290420bc7b00c52c754d
Travis Weadock <twead87@gmail.com>
2020-07-01 12:01:28 -0400

    Figured out a new approach to question 2 using a loop and nested if. commented out the previous attempt.

--- Program.cs
+++ Program.cs
@@ -10,32 +10,67 @@ namespace Assignment1
     class Program
     {
         static void Main(string[] args)
-
-        //question 2
+            //updated solution to #2, using a nested if inside of a foreach loop to catch any punctuation
         {
-
-            string myStr = "Rocky was here";
-            Console.WriteLine("my input is: " + myStr);
-            string rev = StringReverse(myStr);
-
-            Console.WriteLine("reversed string is: " + rev);
-            Console.ReadLine();
-        }
-        
-        private static string StringReverse(string s)
-        {
-            string returnValue = "";
-            //set length equal to 1 minus the string length to avoid errors
-            int len = s.Length - 1;
-            //take the string starting with the highest number and place them until the end, moving down one number at a time.
-            while (len >= 0)
-            {
-                returnValue = returnValue + s[len];
-                len--;
+            Console.WriteLine("Enter a string: ");
+            string entry = Console.ReadLine();
+            string strrev = "";
+            string ph = "";
+            //setting up an array of common punctuation to look for
+            char[] punc = ".,?!".ToCharArray();
+            foreach (char ch in entry)
+            {   //maintaining the whitespace of the string entry
+                if (ch != ' ')
+                {   //if the character does not exist in the punctuation array, the placeholder is set to the character is put at the start of the string, if it does exist it is put at the end.
+                    if (Array.IndexOf(punc, ch) == -1)
+                        ph = ch + ph;
+                    else
+                        ph = ph + ch;
+                    //Console.WriteLine(ph);
+                }
+                else
+                {   //set the final text store equal to itself, plus the word from the place holder, then reset placeholder to blank
+                    strrev = strrev + ph + " ";
+                    ph = "";
+                }
             }
+            strrev = strrev + ph + " ";
+            Console.WriteLine(strrev);
+            Console.ReadLine();
 
-            return returnValue;
+
         }
+
+
     }
+
+    //static void Main(string[] args)
+
+    ////question 2
+    //{
+
+    //    string myStr = "Rocky was here";
+    //    Console.WriteLine("my input is: " + myStr);
+    //    string rev = StringReverse(myStr);
+
+    //    Console.WriteLine("reversed string is: " + rev);
+    //    Console.ReadLine();
+    //}
+
+    //private static string StringReverse(string s)
+    //{
+    //    string returnValue = "";
+    //    //set length equal to 1 minus the string length to avoid errors
+    //    int len = s.Length - 1;
+    //    //take the string starting with the highest number and place them until the end, moving down one number at a time.
+    //    while (len >= 0)
+    //    {
+    //        returnValue = returnValue + s[len];
+    //        len--;
+    //    }
+
+    //    return returnValue;
 }
+   
+
     
