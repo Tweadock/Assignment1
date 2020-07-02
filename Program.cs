c10526e1362cb13db0b351a6dc740f86e96be354
Luis Nakano <lgnakano@gmail.com>
2020-07-01 21:32:08 -0400

    Solution that matches O(n).

--- Program.cs
+++ Program.cs
@@ -13,13 +13,24 @@ namespace Assignment1
         {   //problem #3 
             //creating the array, length argument, and function.
             char[] arr = { 'a', 'b', 'c', 'a', 'b', 'c' };
-            int k = 2;
+            int k = 3;
             Console.WriteLine(ContainsDuplicate(arr, k));
             Console.ReadLine();
-        }
+}
         static bool ContainsDuplicate(char[] arr, int n)
         {
             bool returnValue = false;
+            int i = 0;
+            while ((!returnValue) && (i < arr.Length))
+            {
+                int j = i + 1;
+                while ((!returnValue) && (j <= i + n) && (j < arr.Length))
+                {
+                    returnValue = (arr[i] == arr[j]);
+                    j++;
+                }
+                i++;
+            }
             return returnValue;
         }
     }
