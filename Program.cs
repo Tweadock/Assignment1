52b1f2a4fffb8098d957bdc44fa0807b2e2b7149
Luis Nakano <lgnakano@gmail.com>
2020-07-05 17:21:12 -0400

    Adding exercise 3 to develop.

--- Program.cs
+++ Program.cs
@@ -24,6 +24,11 @@ namespace Assignment1
             string strrev = StringReverse(entry);
             Console.WriteLine(strrev);
             Console.ReadLine();
+
+            //creating the array, length argument, and function.
+            int[] arr2 = { 2, 2, 3, 5, 6 };
+            int n = arr2.Length;
+            Console.WriteLine(minSum(arr2, n));
         }
 
         static int[] targetRange(int[] marks, int target)
@@ -76,6 +81,35 @@ namespace Assignment1
             }
             strrev = strrev + ph + " ";
             return strrev;
+        }
+
+        static int minSum(int[] arr, int n)
+        {   //setting the sum of the array to start at the first number
+            int sum = arr[0];
+            //creating a loop to run as long as the length of the array, starting at position 1 because position 0 is already accounted for
+            int i = 1;
+            while (i < n)
+            {   //if i is the same as the number before it, run the while loop to check for while the two numbers are equal, make the number i + 1, then add position i to the sum
+                if (arr[i] == arr[i - 1])
+                {
+                    // propagate the change until the updated number does not match next,
+                    // or until the end of the string, in case the new number matches the next
+                    // needs to be a loop because the next updated number could go to multiple positions
+                    // on the array
+                    while ((i < n) && (arr[i] <= arr[i - 1]))
+                    {
+                        arr[i] = arr[i] + 1;
+                        sum = sum + arr[i];
+                        i++;
+                    }
+                }
+                else
+                {
+                    sum = sum + arr[i];
+                    i++;
+                }
+            }
+            return sum;
         }
     }
 }
