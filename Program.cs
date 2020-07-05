eb149032e68981f5727aaea54c1538a280d56709
Luis Nakano <lgnakano@gmail.com>
2020-07-05 16:10:13 -0400

    Setting up for a single main function calling all other functions.

--- Program.cs
+++ Program.cs
@@ -10,18 +10,16 @@ namespace Assignment1
     class Program
     {
         static void Main(string[] args)
-
-        ////Problem 1
-        ////set up the initial array and the target number
         {
+            ////Problem 1
+            ////set up the initial array and the target number
             int[] arr = { 1, 2, 3, 4, 5, 6, 6, 6, 7, 9 };
             int x = 6;
-            int[] getArrayResult = GetArray(arr, x);
-            Console.WriteLine(getArrayResult[0]);
-            Console.WriteLine(getArrayResult[1]);
+            int[] getArrayResult = targetRange(arr, x);
+            Console.WriteLine("Problem 1 Response: [" + getArrayResult[0] + ",  " + getArrayResult[1]);
         }
 
-        static int[] GetArray(int[] marks, int target)
+        static int[] targetRange(int[] marks, int target)
 
         ////set up the function, plus the first and last numbers.
         ////with the for loop, say that if the target number is not equal to i, move on, if not and first is == -1, set it equal to the target, then continue to make last equal to i until the end
